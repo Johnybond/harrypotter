394dd50b358f7a070c7729ed6bb5c172237bf943
Johnybond <johny.vega.07@gmail.com>
2013-11-26 16:16:43 -0600

    1er Commit

---
+++ TDDCourse_IMP0047/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz 
+    {
+        public string Answer(int input)
+        {
+            return input.ToString();
+        }
+    }
+}
