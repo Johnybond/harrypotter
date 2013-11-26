8a1016b0778484807f87a9fcd187f958fb63a420
Johnybond <johny.vega.07@gmail.com>
2013-11-26 16:55:34 -0600

    Comit modulos

--- TDDCourse_IMP0047/FizzBuzz.Tests/FizzBuzzTests.cs
+++ TDDCourse_IMP0047/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -9,13 +9,34 @@ namespace FizzBuzz.Tests
     [TestFixture]
     public class FizzBuzzTests
     {
-        [Test]
-        public void Answer_InputEqual1_OutputEqual1()
+        private FizzBuzz _fizzBuzz;
+
+        [SetUp]
+        public void Setup()
         {
-            string expectedOutPut="1";
-            FizzBuzz fb = new FizzBuzz ();
-            string output = fb.Answer(1);
-            Assert.AreEqual(expectedOutPut, output);
+            this._fizzBuzz = new FizzBuzz();
+        }
+
+        [Test]
+        [TestCase(1,"1")]
+        [TestCase(2,"2")]
+        [TestCase(3,"fizz")]
+        [TestCase(4, "4")]
+        [TestCase(5, "buzz")]
+
+         public void Answer_InputEqualValue_OutputCorrect(int input, string expectedOutput)
+        {
+            string output = this._fizzBuzz.Answer(input);
+            if ((input % 3)==0)
+            {
+                output= "fizz"; 
+            }
+            if ((input % 5) == 0)
+            {
+                output = "buzz";
+            }
+            Assert.AreEqual(expectedOutput, output);
+
         }
     }
 }
